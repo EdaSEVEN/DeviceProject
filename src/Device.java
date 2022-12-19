7fee178a5403dbd61094c23ab1ae49c902d1a81b
nevzattalhaozcan <ozcann.talha@gmail.com>
2022-12-19 17:51:15 +0300

    Created Device.java

---
+++ src/Device.java
@@ -0,0 +1,46 @@
+public class Device {
+
+    private String brand, model;
+    private double price;
+    private boolean hasBattery, hasPowerButton;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isHasBattery() {
+        return hasBattery;
+    }
+
+    public boolean isHasPowerButton() {
+        return hasPowerButton;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setHasBattery(boolean hasBattery) {
+        this.hasBattery = hasBattery;
+    }
+
+    public void setHasPowerButton(boolean hasPowerButton) {
+        this.hasPowerButton = hasPowerButton;
+    }
+}
